8721682b502ba81a1a07d8194073e03549131563
noomenbm <noomen.belmechri@gmail.com>
2025-07-14 11:40:57 -0700

    Finished Part 7

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tcod
+import color
 import copy
 from entity import Entity
 from engine import Engine
@@ -11,7 +12,7 @@ def main() -> None:
     screen_height = 50
 
     map_width = 80
-    map_height = 45
+    map_height = 43
 
     room_max_size = 10
     room_min_size = 6
@@ -38,6 +39,9 @@ def main() -> None:
     )
     engine.update_fov()
 
+    engine.message_log.add_message("Hello and welcome, adventurer, to yet another dungeon!", color.welcome_text, )
+
+
     with tcod.context.new(
         columns=screen_width,
         rows=screen_height,
@@ -47,8 +51,10 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
-            engine.render(console=root_console, context=context)
-            engine.event_handler.handle_events()
+            root_console.clear()
+            engine.event_handler.on_render(console=root_console)
+            context.present(root_console)
+            engine.event_handler.handle_events(context)
 
 if __name__ == "__main__":
     main()
